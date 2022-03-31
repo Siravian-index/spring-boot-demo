cbaad518ab9979ef923bbb77dde2654892380f42
David <depch47@gmail.com>
2022-03-31 15:27:56 -0500

    post method working

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,8 +3,8 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -21,6 +21,11 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
+        var studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("Email taken");
+        }
         System.out.println(student);
+        studentRepository.save(student);
     }
 }
